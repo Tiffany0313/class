73b9350b34964e9c62b12f6bb53fba5fba2c0b99
Tiffany0313 <pinky95123@gmail.com>
2021-10-19 23:04:00 +0800

    第九週

--- src/ui/AppMenu.js
+++ src/ui/AppMenu.js
@@ -4,30 +4,32 @@ import { NavLink } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import { makeStyles } from "@mui/styles";
+import { useTheme } from "@mui/material/styles";
+// import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles((theme) => ({
-    active: {
-        backgroundColor: theme.palette.secondary.main,
-        color: theme.palette.text.secondary
-    }
-}));
+// const useStyles = makeStyles((theme) => ({
+//     active: {
+//         backgroundColor: theme.palette.secondary.main,
+//         color: theme.palette.text.secondary
+//     }
+// }));
 
 export default function Main() {
-    const classes = useStyles();
-
+    // const classes = useStyles();
+    const theme = useTheme();
+    const activeStyle = { backgroundColor: theme.palette.secondary.main, color: 'black' }
     return (
         <AppBar position="sticky">
             {/* Toolbar水平排列 */}
             <Toolbar>
                 <Button>
-                    {/* NavLink會去比對現在的頁面位置，當位置為我們設定的連結，就會套用activeClassName */}
-                    <NavLink to="/employee" activeClassName={classes.active}>
+                    {/* NavLink會去比對現在的頁面位置，當位置為我們設定的連結，就會套用activeStyle */}
+                    <NavLink to="/employee" activeStyle={activeStyle}>
                         LINK to employee
                     </NavLink>
                 </Button>
                 <Button>
-                    <NavLink to="/product" activeClassName={classes.active}>
+                    <NavLink to="/product" activeStyle={activeStyle}>
                         LINK to product
                     </NavLink>
                 </Button>
--- src/ui/Main.js
+++ src/ui/Main.js
@@ -7,39 +7,40 @@ import SignIn from "../account/SignIn";
 
 // import { useHistory } from "react-router-dom";
 
-import firebase from "firebase/app";
-
 export default function Main() {
-  const [status, setStatus] = useState("signIn");
+    const [status, setStatus] = useState("signIn");
 
-  // const history = useHistory();
-  // useHistory 可以直接指定某頁面
-  // const handleClick = () => {
-  //   history.push("/product");
-  // };
-  return (
-    <>
-      {/* && history.push("/product") */}
-      {status === "signIn" ? (
-        <SignIn setStatus={setStatus} />
-      ) : status === "signUp" ? (
-        <SignUp setStatus={setStatus} />
-      ) : (
-        <Tab setStatus={setStatus} />
-        // <SignOut setStatus={setStatus} />
-      )}
-      {/* <div
-        style={{
-          height: "90vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <Button variant="contained" color="secondary" onClick={handleClick}>
-          Product
-        </Button>
-      </div> */}
-    </>
-  );
+    // const history = useHistory();
+    // useHistory 可以直接指定某頁面
+    // const handleClick = (link) => {
+    //   history.push(link);
+    // };
+    return (
+        <>
+            {/* && history.push("/product") */}
+            {status === "signIn" ? (
+                <SignIn setStatus={setStatus} />
+            ) : status === "signUp" ? (
+                <SignUp setStatus={setStatus} />
+            ) : (
+                <Tab setStatus={setStatus} />
+                // <SignOut setStatus={setStatus} />
+            )}
+            {/* <div
+                style={{
+                    height: "90vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
+                <Button variant="contained" color="primary" onClick={() => handleClick("/product")}>
+                    Product
+                </Button>
+                <Button variant="contained" color="secondary" onClick={() => handleClick("/employee")}>
+                    Employee
+                </Button>
+            </div> */}
+        </>
+    );
 }
